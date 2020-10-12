815916b85e7e49ddce0a78dba140693d21df3112
Ryan Vosburg <ryan_vosburg1@baylor.edu>
2020-10-12 15:55:41 -0500

    window made

---
+++ alien_invasion.py
@@ -0,0 +1,28 @@
+import sys
+import pygame
+
+class AlienInvasion:
+    """overall class to manage game assets and behavior."""
+
+    def __init__(self):
+        '''initialize the game and create game resources'''
+        pygame.init()
+
+        self.screen = pygame.display.set_mode((1200,800))
+        pygame.display.set_caption("Alien Invasion")
+
+    def run_game(self):
+        '''start main loop for game'''
+        while True:
+            #watch for keyboard and mouse events
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+        
+        #make most recently drawn screen visible
+        pygame.display.flip()
+
+if __name__ == '__mai__':
+    #make a game instance and run the game
+    ai = AlienInvasion()
+    ai.run_game()
