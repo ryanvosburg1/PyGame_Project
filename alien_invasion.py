640ef6bef083946ec08ad2a65021a573c82ce1ee
Ryan Vosburg <ryan_vosburg1@baylor.edu>
2020-10-13 10:02:20 -0500

    add row of aliens

--- alien_invasion.py
+++ alien_invasion.py
@@ -31,6 +31,19 @@ class AlienInvasion:
         #create fleet of aliens
         #make an alien
         alien = Alien(self)
+        alien_width = alien.rect.width
+        available_space_x = self.settings.screen_width - (2 * alien_width)
+        number_aliens_x = available_space_x // (2 * alien_width) #space between each alien is 1 alien width
+
+        #create first row of aliens
+        for alien_number in range(number_aliens_x):
+            self._create_alien(alien_number)
+     
+    def _create_alien(self,alien_number): #create alien and place it in row
+        alien = Alien(self)
+        alien_width = alien.rect.width
+        alien.x = alien_width + 2 * alien_width * alien_number
+        alien.rect.x = alien.x
         self.aliens.add(alien)#add is like append
 
 
